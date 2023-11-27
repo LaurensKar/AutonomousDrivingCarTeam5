9d94a8f5b8dd457114e705c3c17a23b4b5cfdf2a
Gillesw <77160203+GillesWi@users.noreply.github.com>
2023-11-27 15:52:51 +0100

    Add files via upload

--- drive.py
+++ drive.py
@@ -19,17 +19,14 @@ FinetuningFile = 'finetuning.pic'  # Those are the parameters that the filters w
 
 # -------------------------------------------------- Functions ---------------------------------------------
 
-# This function is used to easily finetune our parameters.
-def finetuning(image):
-    cv2.imshow('Input image', image)
 
+def finetuning(image):
     if Finetune:
         if os.path.isfile(FinetuningFile):
             with open(FinetuningFile, 'rb') as f:
                 parameters = pickle.load(f)
                 low_threshold, high_threshold, rho, theta, threshold, minLineLength, maxLineGap = parameters
         else:
-
             cv2.namedWindow('Image', cv2.WINDOW_NORMAL)
             cv2.createTrackbar('Low threshold', 'Image', 0, 500, lambda _: None)
             cv2.createTrackbar('High threshold', 'Image', 0, 500, lambda _: None)
@@ -45,25 +42,45 @@ def finetuning(image):
 
             cv2.destroyWindow('Image')
 
-            cv2.namedWindow('Hough', cv2.WINDOW_NORMAL)
-            cv2.createTrackbar('rho', 'Hough', 1, 10, lambda _: None)
-            cv2.createTrackbar('theta', 'Hough', 1, 360, lambda _: None)
-            cv2.createTrackbar('threshold', 'Hough', 1, 500, lambda _: None)
-            cv2.createTrackbar('minLineLength', 'Hough', 1, 100, lambda _: None)
-            cv2.createTrackbar('maxLineGap', 'Hough', 1, 100, lambda _: None)
+            cv2.namedWindow('Hough Lines', cv2.WINDOW_NORMAL)
+            cv2.createTrackbar('rho', 'Hough Lines', 1, 10, lambda _: None)
+            cv2.createTrackbar('theta', 'Hough Lines', 1, 360, lambda _: None)
+            cv2.createTrackbar('threshold', 'Hough Lines', 1, 500, lambda _: None)
+            cv2.createTrackbar('minLineLength', 'Hough Lines', 1, 100, lambda _: None)
+            cv2.createTrackbar('maxLineGap', 'Hough Lines', 1, 100, lambda _: None)
 
             while True:
-                rho = cv2.getTrackbarPos('rho', 'Hough')
-                theta = cv2.getTrackbarPos('theta', 'Hough') * np.pi / 180
-                threshold = cv2.getTrackbarPos('threshold', 'Hough')
-                minLineLength = cv2.getTrackbarPos('minLineLength', 'Hough')
-                maxLineGap = cv2.getTrackbarPos('maxLineGap', 'Hough')
+                rho = cv2.getTrackbarPos('rho', 'Hough Lines')
+                theta = cv2.getTrackbarPos('theta', 'Hough Lines') * np.pi / 180
+                threshold = cv2.getTrackbarPos('threshold', 'Hough Lines')
+                minLineLength = cv2.getTrackbarPos('minLineLength', 'Hough Lines')
+                maxLineGap = cv2.getTrackbarPos('maxLineGap', 'Hough Lines')
 
-                lines = cv2.HoughLinesP(edges, rho, theta, threshold, minLineLength, maxLineGap)
-                cv2.imshow('Hough Lines', lines)
+                # lines = cv2.HoughLinesP(edges, rho, theta, threshold, minLineLength, maxLineGap)
+
+                lines = cv2.HoughLinesP(edges, rho=1, theta=np.pi / 180, threshold=100, minLineLength=10,
+                                        maxLineGap=250)
+
+                # drawing lines on the image
+                if lines is not None:
+                    line_img = np.zeros_like(image)
+                    for line in lines:
+                        for x1, y1, x2, y2 in line:
+                            cv2.line(line_img, (x1, y1), (x2, y2), (255, 255, 255), 1)
+                    cv2.imshow('Hough Lines', line_img)  # fixed the args to cv2.imshow
+
+                image_to_show = np.copy(image)
+                if lines is not None:
+                    for line in lines:
+                        for x1, y1, x2, y2 in line:
+                            cv2.line(image_to_show, (x1, y1), (x2, y2), (0, 255, 0), 3)
+
+                cv2.imshow('Hough Lines', image_to_show)  # fix: use image as input, not lines
 
                 if cv2.waitKey(1) == ord('q'):
                     break
+
+            cv2.destroyWindow('Hough Lines')
 
             with open(FinetuningFile, 'wb') as f:
                 pickle.dump((low_threshold, high_threshold, rho, theta, threshold, minLineLength, maxLineGap), f)
@@ -83,7 +100,7 @@ def finetuning(image):
             minLineLength = 10
             maxLineGap = 10
 
-    edges = cv2.Canny(edges, low_threshold, high_threshold)
+    edges = cv2.Canny(image, low_threshold, high_threshold)
     lines = cv2.HoughLinesP(edges, rho, theta, threshold, minLineLength, maxLineGap)
 
     return edges, lines
@@ -93,10 +110,6 @@ def calculate_steering_angle(image):
     global plot_shown
     fig, axs = plt.subplots(3)
 
-    # We grayscale our images because of simplicity, computational efficiency and edge strength.
-    # Having one channel instead of 3 (red, green and blue) makes it easier to process and analyze the image. It is also
-    # computationally more efficient. Lastly grayscaling makes sure that all pixels have the same intensity level, this is helpful
-    # because edges are defined by abrupt changes in intensity or color, so by removing color it only focuses on intensity level.
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
     edges, lines = finetuning(gray)
@@ -115,26 +128,13 @@ def calculate_steering_angle(image):
         axs[2].imshow(line_img, cmap='gray')
         axs[2].set_title('Lines')
 
-    # When you visualize a road going off into the distance the 2 edges of the road go towards eachother.
-    # This is what we will use to calculate the driving angle that our car needs to steer towards.
-    # First we find the slope of the edges of the racetrack
+    # Find the slope of the line
     slopes = [(y2 - y1) / (x2 - x1) if (x2 - x1) != 0 else 0 for line in lines for x1, y1, x2, y2 in line]
-    # Convert the slopes into angles measured in radians which we later use to make calculations
     radian_angles = [np.arctan(x) for x in slopes]
 
-    # Of course our filters and line transform aren't perfect and we will end up with quite some horizontal lines too.
-    # We know however that the edges of our racetrack (the only lines we want) will almost never be horizontal and so we filter out all
-    # lines that are less than 15 degrees.
-    threshold_angle_degrees = 15
+    # Remove near horizontal lines based on a threshold
+    radian_angles = [x for x in radian_angles if abs(x) > np.pi / 12]
 
-    # Convert the threshold angle to radians so we can calculate with it
-    threshold_angle_radians = np.deg2rad(threshold_angle_degrees)
-
-    # Remove near horizontal lines based on the constant threshold
-    radian_angles = [x for x in radian_angles if abs(x) > threshold_angle_radians]
-
-    # We now figure out the steering angle by calculating the mean of the slopes of the lines (hopefully only the edges of our racetrack)
-    # that still remain. This angle is where we want to steer our car towards.
     # Take the mean of the angles which is our steering direction
     steering_angle_radian = np.mean(radian_angles)
 
@@ -148,7 +148,7 @@ def calculate_steering_angle(image):
 
     return steering_angle_degree
 
-# Send the steering angle and the throttle to Udacity.
+
 def send_control(steering_angle, throttle):
     sio.emit('steer', data={
         'steering_angle': str(steering_angle),
@@ -166,8 +166,8 @@ def connect(sid, environ):
     print('Connected')
     send_control(0, 0)
 
-# This funtion processes telemetry data received from the simulator and sends control commands back to the simulator
-@sio.on('telemetry') 
+
+@sio.on('telemetry')
 def telemetry(sid, data):
     print('Sending control')
     image_str = data["image"]
@@ -177,6 +177,7 @@ def telemetry(sid, data):
     send_control(angle, 50)
 
 
+# -------------------------------------------------- Code ---------------------------------------------
 
 app = socketio.WSGIApp(sio)
 eventlet.wsgi.server(eventlet.listen(('', 4567)), app)
