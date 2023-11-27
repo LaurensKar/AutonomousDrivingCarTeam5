a0da4c589504341e8a8ab352bd43bc319697ec29
Gillesw <77160203+GillesWi@users.noreply.github.com>
2023-11-27 14:21:04 +0100

    Add files via upload

--- drive.py
+++ drive.py
@@ -1,3 +1,5 @@
+import os.path
+import pickle
 import socketio
 import eventlet
 import base64
@@ -9,28 +11,97 @@ import matplotlib.pyplot as plt
 
 # -------------------------------------------------- Settings ---------------------------------------------
 
-Finetune = False
-plot_shown = False
+Finetune = True  # Specify if you want to finetune the filter again
+plot_shown = False  # Choose if you want to show a summary plot or not
 
-FinetuningFile = 'finetune.pic'
+FinetuningFile = 'finetuning.pic'  # Those are the parameters that the filters will use
 
 
 # -------------------------------------------------- Functions ---------------------------------------------
+
+
+def finetuning(image):
+    cv2.imshow('Input image', image)
+
+    if Finetune:
+        if os.path.isfile(FinetuningFile):
+            with open(FinetuningFile, 'rb') as f:
+                parameters = pickle.load(f)
+                low_threshold, high_threshold, rho, theta, threshold, minLineLength, maxLineGap = parameters
+        else:
+
+            cv2.namedWindow('Image', cv2.WINDOW_NORMAL)
+            cv2.createTrackbar('Low threshold', 'Image', 0, 500, lambda _: None)
+            cv2.createTrackbar('High threshold', 'Image', 0, 500, lambda _: None)
+
+            while True:
+                low_threshold = cv2.getTrackbarPos('Low threshold', 'Image')
+                high_threshold = cv2.getTrackbarPos('High threshold', 'Image')
+                edges = cv2.Canny(image, low_threshold, high_threshold)
+                cv2.imshow('Image', edges)
+
+                if cv2.waitKey(1) == ord('q'):
+                    break
+
+            cv2.destroyWindow('Image')
+
+            cv2.namedWindow('Hough', cv2.WINDOW_NORMAL)
+            cv2.createTrackbar('rho', 'Hough', 1, 10, lambda _: None)
+            cv2.createTrackbar('theta', 'Hough', 1, 360, lambda _: None)
+            cv2.createTrackbar('threshold', 'Hough', 1, 500, lambda _: None)
+            cv2.createTrackbar('minLineLength', 'Hough', 1, 100, lambda _: None)
+            cv2.createTrackbar('maxLineGap', 'Hough', 1, 100, lambda _: None)
+
+            while True:
+                rho = cv2.getTrackbarPos('rho', 'Hough')
+                theta = cv2.getTrackbarPos('theta', 'Hough') * np.pi / 180
+                threshold = cv2.getTrackbarPos('threshold', 'Hough')
+                minLineLength = cv2.getTrackbarPos('minLineLength', 'Hough')
+                maxLineGap = cv2.getTrackbarPos('maxLineGap', 'Hough')
+
+                lines = cv2.HoughLinesP(edges, rho, theta, threshold, minLineLength, maxLineGap)
+                cv2.imshow('Hough Lines', lines)
+
+                if cv2.waitKey(1) == ord('q'):
+                    break
+
+            with open(FinetuningFile, 'wb') as f:
+                pickle.dump((low_threshold, high_threshold, rho, theta, threshold, minLineLength, maxLineGap), f)
+
+    else:
+        if os.path.isfile(FinetuningFile):
+            with open(FinetuningFile, 'rb') as f:
+                parameters = pickle.load(f)
+            low_threshold, high_threshold, rho, theta, threshold, minLineLength, maxLineGap = parameters
+        else:
+            print("No finetuning file found. Set finetuning to TRUE. Sending default values")
+            low_threshold = 50
+            high_threshold = 150
+            rho = 1
+            theta = np.pi / 180
+            threshold = 15
+            minLineLength = 10
+            maxLineGap = 10
+
+    edges = cv2.Canny(edges, low_threshold, high_threshold)
+    lines = cv2.HoughLinesP(edges, rho, theta, threshold, minLineLength, maxLineGap)
+
+    return edges, lines
+
 
 def calculate_steering_angle(image):
     global plot_shown
     fig, axs = plt.subplots(3)
 
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    edges, lines = finetuning(gray)
+
     axs[0].imshow(gray, cmap='gray')
     axs[0].set_title('Gray Image')
 
-    edges = cv2.Canny(gray, 100, 200)   # increased thresholds for edge detection
     axs[1].imshow(edges, cmap='gray')
     axs[1].set_title('Edges')
-
-    lines = cv2.HoughLinesP(edges, 1, np.pi/180, 100, minLineLength=60, maxLineGap=300)   
-
     # drawing lines on the image
     if lines is not None:
         line_img = np.zeros_like(image)
@@ -41,12 +112,11 @@ def calculate_steering_angle(image):
         axs[2].set_title('Lines')
 
     # Find the slope of the line
-    # Since the slope of the line will give us an idea of the angle of the road
-    slopes = [(y2 - y1)/(x2 - x1) if (x2-x1) != 0 else 0 for line in lines for x1,y1,x2,y2 in line]
+    slopes = [(y2 - y1) / (x2 - x1) if (x2 - x1) != 0 else 0 for line in lines for x1, y1, x2, y2 in line]
     radian_angles = [np.arctan(x) for x in slopes]
 
     # Remove near horizontal lines based on a threshold
-    radian_angles = [x for x in radian_angles if abs(x) > np.pi/12]
+    radian_angles = [x for x in radian_angles if abs(x) > np.pi / 12]
 
     # Take the mean of the angles which is our steering direction
     steering_angle_radian = np.mean(radian_angles)
@@ -57,10 +127,8 @@ def calculate_steering_angle(image):
     if not plot_shown:
         fig.tight_layout()  # adjusting layout to avoid overlaps
         plt.show()  # show the plot
+        plot_shown = True  # After showing the plot, set the flag to True
 
-        # After showing the plot, set the flag to True
-        plot_shown = True
-        
     return steering_angle_degree
 
 
@@ -75,6 +143,7 @@ def send_control(steering_angle, throttle):
 
 sio = socketio.Server()
 
+
 @sio.on('connect')  # Connect to the game
 def connect(sid, environ):
     print('Connected')
@@ -86,18 +155,12 @@ def telemetry(sid, data):
     print('Sending control')
     image_str = data["image"]
     image = Image.open(BytesIO(base64.b64decode(image_str)))
-    
+
     angle = calculate_steering_angle(np.array(image))
     send_control(angle, 50)
-    
-    
+
+
 # -------------------------------------------------- Code ---------------------------------------------
 
 app = socketio.WSGIApp(sio)
 eventlet.wsgi.server(eventlet.listen(('', 4567)), app)
-
-
-
-
-
-
